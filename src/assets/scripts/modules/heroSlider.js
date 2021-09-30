2143734a56723ed5c9c75ffbeaeceaa3ee12db15
burdms <burdms@gmail.com>
2021-09-30 14:43:17 +0200

    create: hero slider

--- src/assets/scripts/modules/heroSlider.js
+++ src/assets/scripts/modules/heroSlider.js
@@ -2,5 +2,13 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 export default function heroSlider() {
-  console.log(1);
+  if (document.querySelectorAll('.swiper-slide').length > 1) {
+    const heroSlider = new Swiper('.hero-slider', {
+      effect: 'fade',
+      loop: true,
+      autoplay: {
+        delay: 5000,
+      },
+    });
+  }
 }
