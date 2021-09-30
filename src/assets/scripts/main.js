d6ff3b9ce44115066f223493156268f7009686ce
burdms <burdms@gmail.com>
2021-09-30 13:23:55 +0200

    create: hero section markup

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,3 +1,8 @@
 import headerMenu from "./modules/headerMenu";
+import heroSlider from "./modules/heroSlider";
 
 headerMenu();
+
+if (document.querySelector('.hero')) {
+  heroSlider();
+}
---
+++ src/assets/scripts/modules/heroSlider.js
@@ -0,0 +1,6 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+export default function heroSlider() {
+  console.log(1);
+}
