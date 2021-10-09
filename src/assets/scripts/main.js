133751d41213c589c4c86c2ff715fb93567230d1
burdms <burdms@gmail.com>
2021-10-09 22:41:51 +0200

    craete: smooth scroll to anchor

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,10 +1,12 @@
 import headerMenu from "./modules/headerMenu";
 import headerShrinkLogo from "./modules/headerShrinkLogo";
 import heroSlider from "./modules/heroSlider";
+import smoothScroll from "./modules/smoothScrollToAcnhor";
 import testimonialsSlider from "./modules/testimonialsSlider";
 
 headerMenu();
 headerShrinkLogo();
+smoothScroll();
 
 if (document.querySelector('.hero')) {
   heroSlider();
